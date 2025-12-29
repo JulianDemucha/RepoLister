93a5209bd7fa964abdf33bb4325ddf1d76d68501
JulianDemucha <demuchajulian.it@gmail.com>
2025-12-29 19:07:04 +0100

    fix(Repo): remove isOwner and add boolean fork field to RepoBase isOwner was supposed to be used to check if repo is forked (obviously forked repository is a new repository so owner isnt inherited), instead use 'fork : boolean' field from github api response

--- repolister/src/main/java/com/repolister/Repo.java
+++ repolister/src/main/java/com/repolister/Repo.java
@@ -2,11 +2,8 @@ package com.repolister;
 
 public record Repo(String name, String ownerLogin)
 {
-    public boolean isOwner(String userLogin) {
-        return ownerLogin.equals(userLogin);
-    }
 
-    public record RepoBase(String name, Owner owner){
+    public record RepoBase(String name, Owner owner, boolean fork){
 
         public static Repo toRepo(RepoBase repoBase) {
             return new Repo(repoBase.name, repoBase.getOwnerLogin());
