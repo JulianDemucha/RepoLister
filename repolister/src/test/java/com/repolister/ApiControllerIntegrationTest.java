cf9ae1e6b9f9d8b17c95c5dc0d895eb18c476eff
JulianDemucha <demuchajulian.it@gmail.com>
2025-12-29 21:36:52 +0100

    chore: upgrade back to Spring Boot 4.0.1 and Java 25

--- repolister/src/test/java/com/repolister/ApiControllerIntegrationTest.java
+++ repolister/src/test/java/com/repolister/ApiControllerIntegrationTest.java
@@ -11,8 +11,8 @@ import com.github.tomakehurst.wiremock.junit5.WireMockExtension;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
@@ -104,7 +104,7 @@ public class ApiControllerIntegrationTest {
                 .andDo(print())
                 .andExpect(status().isNotFound())
                 .andExpect(jsonPath("$.message", is(notFoundMessage)))
-                .andExpect(jsonPath("$.status", is("NOT_FOUND")));
+                .andExpect(jsonPath("$.status", is("404 NOT_FOUND")));
     }
 
 
